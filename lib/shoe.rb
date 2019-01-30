76393721c4352b1517c73d4dd6cb678237ae4aca
Angelo Spampinato <spampinatodesign@gmail.com>
2019-01-30 11:51:46 -0500

    Done.

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,14 +1,24 @@
+require "pry"
+
 class Shoe
   attr_accessor :color, :size, :material, :condition
   attr_reader :brand
 
+  BRANDS = []
+
   def initialize(brand)
     @brand = brand
+    BRANDS << brand if BRANDS.include?(brand) == false
   end
 
   def cobble
     self.condition = "new"
     puts "Your shoe is as good as new!"
   end
+
+  # def uniq_brands
+  # 	BRANDS.each_with_index do |brand|
+  # 	end
+  # end
 
 end
